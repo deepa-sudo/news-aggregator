650481e4e11d627785deff204e1930f1da489968
deepa-sudo <72798959+deepa-sudo@users.noreply.github.com>
2024-09-23 18:15:12 +0530

    feat: Display count

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,12 @@ function App() {
             count is {count}
           </button>
           <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
+            The count is {count}
           </p>
         </div>
         <p className="read-the-docs">
           Click on the Vite and React logos to learn more
-          This is a test packge
+          This is a test package
         </p>
       </Layout>
     </>
